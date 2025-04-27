d266b015b3a97b35a5502e819f326756c26756e1
Yuuzito <alfonsogg111@gmail.com>
2025-04-27 18:55:14 -0400

    Se documento la clase Dulce

--- Dulce.java
+++ Dulce.java
@@ -2,19 +2,36 @@ package org.example;
 
 
 abstract class Dulce extends Producto{
-    public Dulce(int b) {
-        super(b);
+    /**
+     * Constructor de Dulce que le da su numero de serie.
+     * @param serie numero de serie que se le asignara.
+     */
+    public Dulce(int serie) {
+        super(serie);
     }
+
+    /**
+     * Metodo necesario de la clase Dulce (es abstracto en la clase Producto)
+     * @return devuelve el tipo de producto que es (Dulce en este caso), luego se le hara override.
+     */
     public String accion(){
-        return "Comida";
+        return "Dulce";
     }
 }
 
 class Super8 extends Dulce {
+    /**
+     * Constructor de Super8 que le da su número de serie, llama al constructor de la superclase.
+     * @param serie numero de serie que se le asignara al Super8.
+     */
     public Super8(int serie) {
         super(serie);
     }
 
+    /**
+     *
+     * @return devuelve que tipo de dulce es, en este caso un Super8.
+     */
     @Override
     public String accion() {
         return "Super8";
@@ -22,10 +39,18 @@ class Super8 extends Dulce {
 }
 
 class Snickers extends Dulce {
+    /**
+     * Constructor de Snickers que le da su número de serie, llama al constructor de la superclase.
+     * @param serie numero de serie que se le asignara al Snickers.
+     */
     public Snickers(int serie) {
         super(serie);
     }
 
+    /**
+     *
+     * @return devuelve que tipo de dulce es, en este caso un Snickers.
+     */
     @Override
     public String accion() {
         return "Snickers";
